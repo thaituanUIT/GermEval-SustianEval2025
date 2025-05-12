3a8d581542d6b78972ddce7063a2afd76ba79205
ThaiTuanUIT <charlotteglorxia2005@gmail.com>
2025-05-12 21:01:12 +0700

    first commit

---
+++ code/modules/preprocess.py
@@ -0,0 +1,6 @@
+from transformers import AutoTokenizer
+import torch
+import pandas as pd
+from read_file import read_jsonl
+
+
---
+++ code/modules/read_file.py
@@ -0,0 +1,9 @@
+import json 
+import pandas as pd
+
+def read_jsonl(file_path):
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            data.append(json.loads(line))
+    return pd.DataFrame(data)
